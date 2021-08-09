f2ace341b32b62ea49a1a6628a09160944d99741
Samúel Jón Gunnarsson <samuel.jon.gunnarsson@gmail.com>
2021-08-09 14:53:55 +0200

    Added Apple M1 binary support

--- build.sh
+++ build.sh
@@ -8,6 +8,10 @@ echo compling for MacOS: release/macos/rest-client
 mkdir -p release/macos
 GOOS=darwin GOARCH=amd64 go build -o release/macos/rest-client main.go
 
+echo compling for MacOS M1: release/macosm1/rest-client
+mkdir -p release/macosm1
+GOOS=darwin GOARCH=arm64 go build -o release/macosm1/rest-client main.go
+
 echo compling for Linux: release/linux/rest-client
 mkdir -p release/linux
 GOOS=linux GOARCH=amd64 go build -o release/linux/rest-client main.go
